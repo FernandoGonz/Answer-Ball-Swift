bdd2a1b80e9413e4030858bbe8d3c1044d4d1369
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-08-03 12:36:16 -0500

    Everything is done and ready to use it

--- Answer Ball/ViewController.swift
+++ Answer Ball/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageBall: UIImageView!
+    let collectionBalls: Array<UIImage> = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball2")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func doAnswer(_ sender: Any) {
+        imageBall.image = collectionBalls.randomElement()
+    }
+    
 }
 
